e9e242e621af2c3ee989a75889f7830bd9a100a8
Jason Liu <jal134@ucsd.edu>
2025-01-24 23:40:44 -0800

    Update global.js

--- global.js
+++ global.js
@@ -20,7 +20,9 @@ for (let p of pages) {
   let url = p.url;
   let title = p.title;
 
-
+  if (!ARE_WE_HOME && !url.startsWith('http')) {
+    url = '../' + url;
+  }
 
   let a = document.createElement('a');
   a.href = url;
