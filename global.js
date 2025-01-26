f408d1eed51d5e0972d79838131d39d0207da0b5
Jason Liu <jal134@ucsd.edu>
2025-01-25 16:59:27 -0800

    Removing Debugging

--- global.js
+++ global.js
@@ -15,7 +15,6 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
-console.log(document.documentElement.classList)
 
 for (let p of pages) {
   let url = p.url;
